407d74a79656dbba661f0c084452845eeae36028
toydev <toy@ownway.info>
2019-05-21 22:33:10 +0900

    Prism.Unity for WPF 7.1 のサンプルとして ViewModelLocator のサンプルを Simple プロジェクトとして作成した。

---
+++ Simple/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+using Prism.Ioc;
+using Prism.Unity;
+
+using Simple.Views;
+
+namespace Simple
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ Simple/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Mvvm;
+
+namespace Simple.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Text1 { get; set; } = "Hello World!";
+    }
+}
